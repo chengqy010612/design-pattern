3e702aa1c01ee27b3a0f00de12ea4049886c9775
程清源 <2033619233@qq.com>
2022-11-12 10:01:38 +0800

    first

---
+++ 1.js
@@ -0,0 +1,23 @@
+
+
+
+function Create (name,age) {
+ this.name = name
+ this.age = age
+}
+
+
+Create.prototype.say = function () {
+    console.log('say')
+}
+
+
+
+class  Person{
+    constructor(name,age) {
+
+    }
+}
+
+let person = new Create('cqy',19)
+console.log(person);
